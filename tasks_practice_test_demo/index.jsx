0c87ae11e65b93b69b8e7992583ddcbdb015128b
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-12 17:21:15 +0500

    Редктировал страничку и отдельно вынес код в jsx

---
+++ tasks_practice_test_demo/index.jsx
@@ -0,0 +1,8 @@
+import React from "https://esm.sh/react@19?dev";
+import ReactDOM from "https://esm.sh/react-dom@19/client?dev";
+ 
+console.log("hello");
+const rootNode = document.getElementById("app");
+const root = ReactDOM.createRoot(rootNode);
+root.render(
+    <h1>Hello METANIT.COM</h1>
