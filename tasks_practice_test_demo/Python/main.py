74f692b7b8e51705ea47936ec605908f1200877f
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-16 18:15:13 +0500

    Объектно-ориентированное программирование

--- tasks_practice_test_demo/Python/main.py
+++ tasks_practice_test_demo/Python/main.py
@@ -1,2 +1,16 @@
-name = input("Введите имя: ")
-print("Привет,", name)
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+        print("Создан человек с именем", self.name)
+
+    def __del__(self):
+        print("Удален человек с именем", self.name)
+
+
+def create_person():
+    tom = Person("Tom")
+
+
+create_person()
+print("Конец программы")
