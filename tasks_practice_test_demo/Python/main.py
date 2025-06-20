d870049d8bbb9089f56ae66723a1b640160ab06c
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-20 10:49:26 +0500

    Python-Перебор кортежей

--- tasks_practice_test_demo/Python/main.py
+++ tasks_practice_test_demo/Python/main.py
@@ -1,30 +1,6 @@
-people = [
-    ["Tom", 29],
-    ["Alice", 33],
-    ["Bob", 27]
-]
+tom = ("Tom", 22, "Google")
 
-# создание вложенного списка
-person = list()
-person.append("Bill")
-person.append(41)
-# добавление вложенного списка
-people.append(person)
-
-print(people[-1])  # ["Bill", 41]
-
-# добавление во вложенный список
-people[-1].append("+79876543210")
-
-print(people[-1])  # ["Bill", 41, "+79876543210"]
-
-# удаление последнего элемента из вложенного списка
-people[-1].pop()
-print(people[-1])  # ["Bill", 41]
-
-# удаление всего последнего вложенного списка
-people.pop(-1)
-
-# изменение первого элемента
-people[0] = ["Sam", 18]
-print(people)  # [ ["Sam", 18], ["Alice", 33], ["Bob", 27]]
+i = 0
+while i < len(tom):
+    print(tom[i])
+    i += 1
