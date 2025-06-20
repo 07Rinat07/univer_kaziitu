a3db6640ea7f88e8d2d94a891061f713381abe7d
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-20 10:51:58 +0500

    Python - Диапазоны или range представляют неизменяемый последовательный набор чисел. Для создания диапазов применяетя range

--- tasks_practice_test_demo/Python/main.py
+++ tasks_practice_test_demo/Python/main.py
@@ -1,6 +1,6 @@
-tom = ("Tom", 22, "Google")
-
-i = 0
-while i < len(tom):
-    print(tom[i])
-    i += 1
+numbers = list(range(10))
+print(numbers)      # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = list(range(2, 10))
+print(numbers)      # [2, 3, 4, 5, 6, 7, 8, 9]
+numbers = list(range(10, 2, -2))
+print(numbers)      # [10, 8, 6, 4]
