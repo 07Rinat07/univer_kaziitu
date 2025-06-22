755b78fdda325d7b4f91d7e52937dc4a8f5d37e2
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-22 17:58:58 +0500

    Python - Pattern matching

--- tasks_practice_test_demo/Python/index.py
+++ tasks_practice_test_demo/Python/index.py
@@ -1,8 +1,16 @@
-first_name = "Tom"
-text = f"Hello, {first_name}."
-print(text)  # Hello, Tom.
+def operation(a, b, code):
+    match code:
+        case 1:
+            return a + b
+        case 2:
+            return a - b
+        case 3:
+            return a * b
+        case _:
+            return 0
 
-name = "Bob"
-age = 23
-info = f"Name: {name}\t Age: {age}"
-print(info)  # Name: Bob  Age: 23
+
+print(operation(10, 5, 1))  # 15
+print(operation(10, 5, 2))  # 5
+print(operation(10, 5, 3))  # 50
+print(operation(10, 5, 4))  # 0
