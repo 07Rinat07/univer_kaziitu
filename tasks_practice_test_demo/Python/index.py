74810488a02a8a664fa05d09da5aff48ee31d00a
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-24 10:43:27 +0500

    Python - Работа с датами и временем

--- tasks_practice_test_demo/Python/index.py
+++ tasks_practice_test_demo/Python/index.py
@@ -1,7 +1,13 @@
-from zipfile import ZipFile
+from datetime import datetime
 
-with ZipFile("metanit.zip", "a") as myzip:
-    # записываем в архив новый файл "hello5.txt"
-    with myzip.open("hello5.txt", "w") as hello_file:
-        encoded_str = bytes("Python...", "UTF-8")
-        hello_file.write(encoded_str)
+deadline = datetime.strptime("22/05/2017", "%d/%m/%Y")
+print(deadline)  # 2017-05-22 00:00:00
+
+deadline = datetime.strptime("22/05/2017 12:30", "%d/%m/%Y %H:%M")
+print(deadline)  # 2017-05-22 12:30:00
+
+deadline = datetime.strptime("05-22-2017 12:30", "%m-%d-%Y %H:%M")
+print(deadline)  # 2017-05-22 12:30:00
+
+# Работа с датами и временем
+# Модуль datetime
