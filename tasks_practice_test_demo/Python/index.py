ef2e35dfb3c328ebb74c0064a21ea8a60ed98726
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-24 10:48:19 +0500

    Python - Сравнение дат

--- tasks_practice_test_demo/Python/index.py
+++ tasks_practice_test_demo/Python/index.py
@@ -1,15 +1,11 @@
-from datetime import timedelta
+from datetime import datetime
 
-three_hours = timedelta(hours=3)
-print(three_hours)  # 3:00:00
-three_hours_thirty_minutes = timedelta(hours=3, minutes=30)  # 3:30:00
-
-two_days = timedelta(2)  # 2 days, 0:00:00
-
-two_days_three_hours_thirty_minutes = timedelta(days=2, hours=3, minutes=30)  # 2 days, 3:30:00
-
-
-# Нередко при работе с датами возникает необходимость добавить к какой-либо дате определенный промежуток времени или,
-# наоборот, вычесть некоторый период. И специально для таких операций в модуле datetime определен класс timedelta.
-# Фактически этот класс определяет некоторый период времени.
-# Для определения промежутка времени можно использовать конструктор timedelta:
+now = datetime.now()
+deadline = datetime(2017, 5, 22)
+if now > deadline:
+    print("Срок сдачи проекта прошел")
+elif now.day == deadline.day and now.month == deadline.month and now.year == deadline.year:
+    print("Срок сдачи проекта сегодня")
+else:
+    period = deadline - now
+    print("Осталось {} дней".format(period.days))
