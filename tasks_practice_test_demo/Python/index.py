3bf083adf3f1c7042ad42d5ba36d033386346c8d
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-24 09:56:44 +0500

    Python - Извлечение файлов

--- tasks_practice_test_demo/Python/index.py
+++ tasks_practice_test_demo/Python/index.py
@@ -1,7 +1,4 @@
 from zipfile import ZipFile
 
-myzip = ZipFile("metanit.zip", "w")
-
-
-# Создание и закрытие файла
-# Для создания архивного файла в конструктор ZipFile передается режим "w" или "a":
+with ZipFile("metanit.zip", "r") as myzip:
+    myzip.extractall()
