2d9c1faf358b3ccccaa06ef28825dd076a8e1f62
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-24 09:57:46 +0500

    Python - Open file

--- tasks_practice_test_demo/Python/index.py
+++ tasks_practice_test_demo/Python/index.py
@@ -1,4 +1,7 @@
 from zipfile import ZipFile
 
-with ZipFile("metanit.zip", "r") as myzip:
-    myzip.extractall()
+with ZipFile("metanit.zip", "a") as myzip:
+    # записываем в архив новый файл "hello5.txt"
+    with myzip.open("hello5.txt", "w") as hello_file:
+        encoded_str = bytes("Python...", "UTF-8")
+        hello_file.write(encoded_str)
