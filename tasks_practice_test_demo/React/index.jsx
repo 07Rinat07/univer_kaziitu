4cbe27b334788ae111abe58ae53e8468e12b7a0a
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-26 14:07:08 +0500

    Js react. Радиокнопки (radibutton)

--- tasks_practice_test_demo/React/index.jsx
+++ tasks_practice_test_demo/React/index.jsx
@@ -1,7 +1,1 @@
-const rootNode = document.getElementById("app");    // элемент для рендеринга приложения React
-// получаем корневой элемент 
-const root = ReactDOM.createRoot(rootNode);
-// рендеринг в корневой элемент
-root.render(
-  <h1>Hello React</h1>,  // элемент, который мы хотим создать
-);
+<input type="radio" value="JavaScript" checked={selectedOption === "JavaScript"} onChange={handleChange}/>
