34f0b286a76182cc2d6cef67a621738b4b58addd
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-26 13:50:12 +0500

    JS React univer lessons. Управление несколькими полями форм

--- tasks_practice_test_demo/React/index.js
+++ tasks_practice_test_demo/React/index.js
@@ -1,6 +1,4 @@
-const rootNode = document.getElementById("app"); // элемент для рендеринга приложения React
-// получаем корневой элемент 
-const root = ReactDOM.createRoot(rootNode);
-// рендеринг в корневой элемент
-root.render(/*#__PURE__*/React.createElement("h1", null, "Hello React") // элемент, который мы хотим создать
-);
+const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+};
