e9865a765744666e032ae7d67c1daf05e433498b
Rinat Sarmuldin <ura07srr@gmail.com>
2025-06-27 14:12:35 +0500

    JS React lessons Маршрутизация Определение маршрутов

--- tasks_practice_test_demo/React/app.js
+++ tasks_practice_test_demo/React/app.js
@@ -1,2 +1,12 @@
-const inputElement = nameField.current;
-console.log("Имя: " + inputElement.value);
+const http = require("http");
+const fs = require("fs");
+   
+http.createServer(function(_, response){
+       
+    fs.readFile("index.html", function(_, data){   
+        response.end(data);
+    });
+     
+}).listen(3000, function(){
+    console.log("Server started at 3000");
+});
