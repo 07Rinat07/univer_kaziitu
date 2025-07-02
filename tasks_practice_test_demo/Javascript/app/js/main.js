64f8b496ad799aa91b175495509c998609ee1af7
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-02 20:29:40 +0500

    Основы JavaScript и повторное прохождение - Замыкания

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,7 +1,20 @@
-{
-    var a = 5;
-    let b = 8;
-    const c = 9;
+function person(name, age){
+    console.log("Person", name, "created");
+ 
+    function print(){
+         console.log("Person ", name, " (" +age +")");
+    }
+    function work(){
+         console.log("Person ", name, " works");
+    }
+    function incrementAge(value){
+        age = age + value;
+    }
+    return [print, work, incrementAge];
 }
-
-/**Определение локальной области видимости */
+ 
+const tom = person("Tom", 39);
+tom[0]();       // print
+tom[1]();       // work
+tom[2](1);      // incrementAge
+tom[0]();       // print
