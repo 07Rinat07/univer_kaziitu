4f6a27fcfc568ee5959dcd73bad63682d476bef6
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-02 20:24:56 +0500

    Основы JavaScript и повторное прохождение - Функциональное программирование

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,2 +1,10 @@
-var username = "Tom";
-let userage = 37;
+function goodMorning(){
+    console.log("Доброе утро");
+}
+function goodEvening(){
+    console.log("Добрый вечер");
+}
+let message = goodMorning;      // присваиваем переменной message функцию goodMorning
+message();      // Доброе утро
+message = goodEvening;          // меняем функцию в переменной message
+message();      // Добрый вечер
