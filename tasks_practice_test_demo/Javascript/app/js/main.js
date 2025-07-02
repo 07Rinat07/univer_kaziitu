85944c12602775ce7656a08d5bb296bb40cb34db
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-02 20:31:56 +0500

    Основы JavaScript и повторное прохождение --- Функции IIFE. Самовызывающиеся функции

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,20 +1,4 @@
-function person(name, age){
-    console.log("Person", name, "created");
- 
-    function print(){
-         console.log("Person ", name, " (" +age +")");
-    }
-    function work(){
-         console.log("Person ", name, " works");
-    }
-    function incrementAge(value){
-        age = age + value;
-    }
-    return [print, work, incrementAge];
-}
- 
-const tom = person("Tom", 39);
-tom[0]();       // print
-tom[1]();       // work
-tom[2](1);      // incrementAge
-tom[0]();       // print
+// самозывающаяся функция
+(function(){
+    console.log("Привет мир");
+}());
