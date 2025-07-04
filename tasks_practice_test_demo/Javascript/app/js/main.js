36ce296c451763c1fcb54d9fc1c94f6c576e2167
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-04 18:52:49 +0500

    Основы JavaScript и повторное прохождение ---Оператор ?.Цепочка проверок

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,8 +1,21 @@
-let nums = [9, 3, 5, 2, 1, 4, 8, 6];
-for(let i = 0; i < nums.length; i++)
-    for(let j = 0; j < nums.length; j++)
-        if (nums[i] < nums[j]) [nums[j], nums[i]] = [nums[i], nums[j]];
+const sam = {name: "Sam"};
+const tom = { 
+    name: "Tom", 
+    company: { title: "Google"}
+};
+const bob = {
+    name: "Bob", 
+    company: {
+        title: "Microsoft",
+        print(){
+            console.log(`Компания ${this.title}`)
+        }
+    }
+};
 
-//**используем деструктуризацию для простейшей сортировки массива: */
-         
-console.log(nums);  // [1, 2, 3, 4, 5, 6, 8, 9]
+
+//**С помощью оператора ?. можно создавать цепочки проверок,
+//  последовательно проверяя, представляет ли значение null/undefined: */
+sam?.company?.print?.();    // не вызывается - нет свойства company
+tom?.company?.print?.();    // не вызывается - нет метода print
+bob?.company?.print?.();    // Компания Microsoft
