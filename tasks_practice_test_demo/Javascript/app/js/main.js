464fd6a50ad578747347cb7dd38b7678117c94ec
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-04 18:46:50 +0500

    Основы JavaScript и повторное прохождение --- Получение оставшихся свойств объекта с помощью rest-оператора

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,9 +1,10 @@
-const user = {
+const tom = {
     name: "Tom",
     age: 24,
     phone: "+367438787",
     email: "tom@gmail.com"
 };
-const {name, email} = user;
+const {name, age, ...contacts} = tom;
 console.log(name);      // Tom
-console.log(email);     // tom@gmail.com
+console.log(age);       // 24
+console.log(contacts);  // {phone: "+367438787", email: "tom@gmail.com"}
