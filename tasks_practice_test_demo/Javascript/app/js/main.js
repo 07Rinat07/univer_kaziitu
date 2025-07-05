7aeb4197dec29a8b3ed252d4e3c77bd9a0244814
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-05 14:20:52 +0500

    Основы JavaScript и повторное прохождение --- Определение конструктора в классе-наследнике и ключевое слово super.

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,29 +1,30 @@
 class Person{
-    name;
-    age;
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
     print(){
         console.log(`Name: ${this.name}  Age: ${this.age}`);
     }
 }
 class Employee extends Person{
-    company;
+     
+    constructor(name, age, company){
+        super(name, age);
+        this.company = company;
+    }
     work(){
         console.log(`${this.name} works in ${this.company}`);
     }
 }
   
-//** После названия класса-наследника ставится ключевое слово extends, после которого идет имя класса, 
-// от которого мы хотим унаследовать функционал.*/
-
-/*Так, изменим классы Person и Employee, применив наследование: */
-
-const tom = new Person();
-tom.name = "Tom"; 
-tom.age= 34;
-const bob = new Employee();
-bob.name = "Bob"; 
-bob.age = 36; 
-bob.company = "Google";
+const tom = new Person("Tom", 34);
 tom.print();    // Name: Tom  Age: 34
-bob.print();    // Name: Bob  Age: 36
-bob.work();     // Bob works in Google
+ 
+
+/**Производный класс также может определить свой конструктор. Если производный класс определяет конструктор, то в нем должен быть вызван конструктор базового класса. Для обращения производном классе к функциональности базового класса, 
+ * в том числе для обращения к конструктору базового класса, применяется ключевое слово super */
+
+const sam = new Employee("Sam", 25, "Google");
+sam.print();    // Name: Sam  Age: 25
+sam.work();     // Sam works in Google
