eb8f13b31726d7e025f5d9dabd7d538cedae4ca9
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-06 19:08:16 +0500

    Основы JavaScript и повторное прохождение --- Комбинация флагов Также можно использовать сразу н

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,6 +1,3 @@
-const exp = /\d\d\d\d\d\d\d\d\d\d/;    // соответствует 10 цифрам подряд
- 
-const phone1 = "+12345678901";
-const phone2 = "42345678901";
-console.log(exp.test(phone1));   // true
-console.log(exp.test(phone2));   // true
+const str = "hello\nWorld";
+const exp = /hello.world/si;
+console.log(exp.test(str)); // true
