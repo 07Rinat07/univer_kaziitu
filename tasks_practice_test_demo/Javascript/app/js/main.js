889619fe8a03678ffcbcbefdd197079a09c1b8e1
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-07 22:05:52 +0500

    Основы JavaScript и повторное прохождение ---Встроенные объекты. Объект Date. Работа с датами

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,27 +1,11 @@
-// определение обертки для обработки ошибок
-function tryCatch (fn){
-  try{
-    return [fn(), null];
-  }
-  catch(err){
-    return [null, err];
-  }
-}
- 
-// использование
-const sqrt = (x) => {
-  if(x < 0) throw new Error(`Number ${x} is invalid`);
-  return Math.sqrt(x);
-}
- 
-// пример получения ошибки
-const [res1, err1] = tryCatch(()=>sqrt(-4));
- 
-if(err1) console.error(err1);
-else console.log("sqrt(-4):", res1);
- 
-// пример получения результата
-const [res2, err2] = tryCatch(()=>sqrt(4));
- 
-if(err2) console.error(err2);
-else console.log("sqrt(4):", res2);
+const date1 = new Date("27 March 2008");
+console.log(date1); // Thu Mar 27 2008 00:00:00 GMT+0300 (Москва, стандартное время)
+// или так
+const date2 = new Date("3/27/2008");
+console.log(date2); // Thu Mar 27 2008 00:00:00 GMT+0300 (Москва, стандартное время)
+// или так
+const date3 = new Date("3 27 2008");
+console.log(date3); // Thu Mar 27 2008 00:00:00 GMT+0300 (Москва, стандартное время)
+
+//**С помошью пустого конструктора Date без параметров. В этом случае созданный объект хранит 
+// текущие дату и время */
