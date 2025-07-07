64f1bfcfd3a1dd9c837ccd23f726170f079bf5ba
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-07 22:18:07 +0500

    Основы JavaScript и повторное прохождение --- Обработка ошибок генератора

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,16 +1,20 @@
-function* getNumber(){
-    const n = yield 5;      // получаем значение numberGenerator.next(2).value
-    console.log("n:", n);
-    const m = yield 5 * n;  // получаем значение numberGenerator.next(3).value
-    console.log("m:", m);
-    yield 5 * m;
-}
-const numberGenerator = getNumber();
- 
-console.log(numberGenerator.next().value);      // 5
-console.log(numberGenerator.next(2).value);     // 10
-console.log(numberGenerator.next(3).value);     // 15
+function* generateData(){
+    try {
+        yield "Tom";
+        yield "Bob";
+        yield "Hello Work";
+    }
+    catch(error) {
+        console.log("Error:", error);
+    }
+} 
+const personGenerator = generateData();
+console.log(personGenerator.next());        // {value: "Tom", done: false}
+personGenerator.throw("Something wrong");   // Error: Something wrong
+console.log(personGenerator.next());        // {value: undefined, done: true}
 
-//Передача данных в метод next
-//** С помощью next() можно передать в генератор данные. Переданные в этот метод данные 
-// можно получить в функции генератора через предыдущий вызов оператора yield:*/
+//Обработка ошибок генератора
+
+//**С помощью функции throw() мы можем сгенерировать в генераторе исключение. 
+// В качестве параметра в эту функцию передается произвольное значение, которое представляет
+//  информацию об ошибке: */
