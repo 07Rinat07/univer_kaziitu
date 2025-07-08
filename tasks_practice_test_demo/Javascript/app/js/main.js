45e869977f5147cf42c97eda2e689e21d6a2aa42
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-08 18:21:15 +0500

    Основы JavaScript и повторное прохождение --- Навигация по DOM. appendChild

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,15 +1,4 @@
-// пустой WeakMap
-const weakMap1 = new WeakMap();
- 
-// WeakMap с инициализацией данными
-let key1 = {key:1};
-let key2 = {key:2};
-let value1 = {name: "Tom"};
-let value2 = {name: "Sam"};
- 
-const weakMap2 = new WeakMap([[key1, value1], [key2, value2]]);
-// или так
-// const weakMap2 = new WeakMap([[{key:1}, {name: "Tom"}], [{key:2}, {name: "Sam"}]]);
-
-//**WeakMap представляет развитие коллекции Map. Особенностью WeakMap является то, что все 
-// ее элементы должны представлять объекты. При этом ключи должны представлять объекты. */
+const header = document.createElement("h1");        // создаем заголовок <h1>
+const  headerText = document.createTextNode("Hello World"); // создаем текстовый узел
+header.appendChild( headerText); // добавляем в элемент h1 текстовый узел
+console.log(header);  // <h1>Hello World</h1>
