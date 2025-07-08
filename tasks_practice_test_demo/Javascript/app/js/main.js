e1218fffe5f31ebe0562a53746a635398cdd1610
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-08 18:25:12 +0500

    Основы JavaScript и повторное прохождение --- Навигация по DOM. у объекта Node можно использовать метод cloneNode():

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,4 +1,2 @@
 const header = document.createElement("h1");        // создаем заголовок <h1>
-const  headerText = document.createTextNode("Hello World"); // создаем текстовый узел
-header.appendChild( headerText); // добавляем в элемент h1 текстовый узел
-console.log(header);  // <h1>Hello World</h1>
+header.textContent = "Hello World"; // определяем текст элемента
