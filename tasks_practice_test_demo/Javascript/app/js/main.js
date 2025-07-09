887b25449152eefd3d17142b98466f930d83b6be
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-09 21:04:26 +0500

    Основы JavaScript и повторное прохождение --- События. События также могут быть нисходящими (capturing). Для их использования в метод addEventListener() в качестве третьего необязательного параметра передается логическое значение true или false. Значение true указывает, что событие нисходящим. По умолчанию все события восходящие.

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,3 +1,13 @@
-function btn_click(e){
-    console.log(this===e.target); // true
-}
+const redRect = document.getElementById("redRect");
+redRect.addEventListener("click", function(){
+    console.log("Событие на redRect");
+}, true);
+ 
+const blueRect = document.getElementById("blueRect");
+blueRect.addEventListener("click", function(){
+    console.log("Событие на blueRect");
+}, true);
+ 
+document.body.addEventListener("click", function(){
+    console.log("Событие на body");
+}, true);
