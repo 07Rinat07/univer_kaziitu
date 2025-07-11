59d7401e2a95ed6eaeab158992d126e133925888
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-11 18:25:29 +0500

    Основы JavaScript ---Сериализация в JSON и десериализация---Для сериализации объекта javascript в json применяется функция JSON.stringify():

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,13 +1,9 @@
-const redRect = document.getElementById("redRect");
-redRect.addEventListener("click", function(){
-    console.log("Событие на redRect");
-}, true);
- 
-const blueRect = document.getElementById("blueRect");
-blueRect.addEventListener("click", function(){
-    console.log("Событие на blueRect");
-}, true);
- 
-document.body.addEventListener("click", function(){
-    console.log("Событие на body");
-}, true);
+// объект javascript
+const user = {
+    name: "Tom",
+    married: false,
+    age: 39
+};
+// объект json
+const serializedUser = JSON.stringify(user);
+console.log(serializedUser); // {"name":"Tom","married":false,"age":39}
