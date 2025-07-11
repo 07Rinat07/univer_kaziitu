28676db12508f0e5e1412a31b278d44bd30db9ad
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-11 18:28:43 +0500

    Основы JavaScript-----Преобразование из строки в XML.

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,10 +1,21 @@
-const user = {
-    name: "Tom",
-    married: false,
-    age: 39
-};
-// сериализация
-const serializedUser = JSON.stringify(user);
-// десериализация
-const tomUser = JSON.parse(serializedUser);
-console.log(tomUser.name); // Tom
+const xmlString = `<?xml version="1.0" encoding="UTF-8" ?> 
+    <users>
+        <user name="Tom" age="39">
+            <company>
+                <title>Microsoft</title>
+            </company>
+        </user>
+        <user name="Bob" age="43">
+            <company>
+                <title>Google</title>
+            </company>
+        </user>
+    </users>`;
+ 
+const domParser = new DOMParser();
+const xmlDOM = domParser.parseFromString(xmlString, "text/xml");
+// обратимся к первому элементу user
+const firstUser = xmlDOM.querySelector("user");
+console.log(firstUser.getAttribute("name"));                  // Tom
+console.log(firstUser.getAttribute("age"));                  // 39
+console.log(firstUser.querySelector("title").textContent);    // Microsoft
