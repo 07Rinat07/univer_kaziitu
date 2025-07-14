b0021e6b1a16e26c8d257d40ec808445f6e5a0f4
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-14 12:12:39 +0500

    JS ---> Async и await

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,14 +1,15 @@
-const promise1 = new Promise((resolve, reject) => {
-    reject("error in promise1");
-    setTimeout(resolve, 500, "Hello");
-});
-const promise2 = new Promise((resolve) => {
-    setTimeout(resolve, 1000, "World");
-});
-Promise.any([promise1, promise2])
-    .then(value => console.log(value))       // World
-    .catch(error => console.log(error));
-
-    //**В данном случае первым выполненным будет промис promise1, однако он завершается с ошибкой. Поэтому в метод then(value => console.log(value)) в качестве value будет передана строка "World" - результат промиса promise2, который успешно завершается.
-
-//Может показаться, что Promise.any() делает то же самое, что и Promise.race(), однако эти функции отличаются в отношении того, как они работают с промисами, которые завершились с ошибкой. Promise.race() возвращает первый завершенный промис, вне зависимости завершился он с ошибкой или нет. А Promise.any() возвращает первый успешно завершенный промис (если такой имеется). Если же все промисы завершились с ошибкой, то генерируется исключение типа AggregateError: */
+function sum(x, y){ 
+    return new Promise(function(resolve){
+        const result = x + y;
+        resolve(result);
+    });
+}
+// сокращенный вариант
+// function sum(x, y){ return Promise.resolve(x + y);}
+ 
+ 
+async function calculate(){
+    const value = await sum(5, 3);
+    console.log("Результат асинхронной операции:", value);
+}
+calculate();    // Результат асинхронной операции: 8
