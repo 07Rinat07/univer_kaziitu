c57cec80f3b2ae041518c41be0f862d313346d2c
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-14 12:01:07 +0500

    Java script --- Выполнение асинхронной операции

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,15 +1,17 @@
-cconst myPromise = new Promise(function(resolve, reject){
+function getNumber(str){
+    const parsed = parseInt(str);
+    if (isNaN(parsed)) throw "Not a number";            // Генерируем ошибку
+    else return parsed;
+}
+const myPromise = new Promise(function(resolve){
     console.log("Выполнение асинхронной операции");
-    const parsed = parseInt("Hello");
-    if (isNaN(parsed)) { 
-        throw "Not a number";           // Генерируем ошибку
-    }
-    resolve(parsed);
+    const result = getNumber("hello");
+    resolve(result);
 });
 myPromise.catch( function(error){
     console.log(error);
 });
 
-//**Здесь парсится в число случайная строка. И если результат парсинга не представляет число, 
-// то с помощью оператора throw генерируем ошибку. Это придет к завершению всей функции с ошибкой. 
-// И в итоге результат будет обработан функцией catch */
+//**Здесь парсинг строки в число вынесен во внешнюю функцию - getNumber, однако при вызове этой 
+// функции в промисе, также из оператора throw возникнет ошибка. И соответственно будет выполняться 
+// функция catch(), где роизойдет обработка ошибки. */
