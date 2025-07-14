ecdd0f8137483f5ca521f78e8abc02cd77b400d4
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-14 12:11:48 +0500

    JS ---> Функция Promise.any()

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,12 +1,14 @@
-const promise1 = new Promise((resolve) => {
+const promise1 = new Promise((resolve, reject) => {
+    reject("error in promise1");
     setTimeout(resolve, 500, "Hello");
 });
 const promise2 = new Promise((resolve) => {
     setTimeout(resolve, 1000, "World");
 });
-Promise.race([promise1, promise2])
-    .then(value => console.log(value))       // Hello
+Promise.any([promise1, promise2])
+    .then(value => console.log(value))       // World
     .catch(error => console.log(error));
 
-    //**Функция Promise.race() также принимает несколько промисов, только возвращает первый
-    //  завершенный промис (вне зависимости завершился от успешно или с ошибкой): */
+    //**В данном случае первым выполненным будет промис promise1, однако он завершается с ошибкой. Поэтому в метод then(value => console.log(value)) в качестве value будет передана строка "World" - результат промиса promise2, который успешно завершается.
+
+//Может показаться, что Promise.any() делает то же самое, что и Promise.race(), однако эти функции отличаются в отношении того, как они работают с промисами, которые завершились с ошибкой. Promise.race() возвращает первый завершенный промис, вне зависимости завершился он с ошибкой или нет. А Promise.any() возвращает первый успешно завершенный промис (если такой имеется). Если же все промисы завершились с ошибкой, то генерируется исключение типа AggregateError: */
