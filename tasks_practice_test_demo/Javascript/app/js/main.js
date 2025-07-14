a8c69e03abbd134e44d5d055f003ba6ed5151134
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-14 12:07:37 +0500

    Java script --- Обработка ошибок в цепочке промисов---- в метод then() также можно передать данные. Но эти данные передаются не из метода finally(), а из предыдущего метода then() или catch():

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,23 +1,24 @@
-const helloPromise = new Promise(function(resolve){
-        resolve("Hello");
-})
- 
-const worldPromise = helloPromise.then(function(value){
-        // возвращаем новое значение
-        return value + " World";
-});
-const metanitPromise = worldPromise.then(function(value){
-        // возвращаем новое значение
-        return value + " from METANIT.COM";
-});
-metanitPromise.then(function(finalValue){
-        // получаем финальное значение
-        console.log(finalValue);    // Hello World from METANIT.COM
-});
-
-//**Одним из примуществом промисов является то, что они позволяют создавать цепочки промисов. 
-// Так, ранее мы рассмотрели применение методов then() и catch() для получения и обработки 
-// результатов и ошибок асинхронной операции. При выполнении эти методы генерируют новый объект 
-// Promise, для которого мы также можем вызвать методы then() и catch(), и, таким образом, построить 
-// цепочку промисов. Благодаря этому мы можем обрабатывать подряд несколько асинхронных операций - 
-// одна за другой. */
+function generateNumber(str){
+    return new Promise((resolve, reject) => {
+        const parsed = parseInt(str);
+        if (isNaN(parsed)) reject("Not a number");
+        else resolve(parsed);
+    });
+};
+function printNumber(str){
+    generateNumber(str)
+    .then(value => {
+        console.log(value);
+        return "hello from then";
+    })
+    .catch(error => {
+        console.log(error);
+        return "hello from catch";
+    })
+    .finally(() => {
+        console.log("End");
+        return "hello from finally";
+    })
+    .then(message => console.log(message));
+}
+printNumber("3");
