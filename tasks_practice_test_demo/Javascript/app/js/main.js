e88c7e189fafefc5448df9b08604e33a1ab2949a
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-19 14:21:25 +0500

    JS - Promise в Ajax-запросах

--- tasks_practice_test_demo/Javascript/app/js/main.js
+++ tasks_practice_test_demo/Javascript/app/js/main.js
@@ -1,10 +1,17 @@
-async function* generatePersonAsync(people){
-    for(const person of people)
-        yield await new Promise(resolve => setTimeout(() => resolve(person), 2000));
+function post(url, data) {
+    return new Promise((succeed, fail) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+        xhr.addEventListener("load", () => {
+            if (xhr.status >=200 && xhr.status < 400)
+                succeed(xhr.response);
+            else
+                fail(new Error(`Request failed: ${xhr.statusText}`));
+        });
+        xhr.addEventListener("error", () => fail(new Error("Network error")));
+        xhr.send(data);
+  });
 }
-async function printPeopleAsync(people){
-    for await (const item of generatePersonAsync(people)) {
-        console.log(item); 
-    }
-}
-printPeopleAsync(["Tom", "Sam", "Bob"]);
+post("http://localhost:3000/user", "Tom")
+    .then(response =>  console.log(response))
+    .catch(error => console.error(error));
