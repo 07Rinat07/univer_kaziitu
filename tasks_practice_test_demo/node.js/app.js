1833841d440bb009ccfc44a5020321a243d0d5f9
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-29 20:45:17 +0500

    Node js ---- Определение конструкторов и объектов в модуле

--- tasks_practice_test_demo/node.js/app.js
+++ tasks_practice_test_demo/node.js/app.js
@@ -1,8 +1,6 @@
-const os = require("os");
-const greeting = require("./greeting");
+// получаем только функцию printMessage
+const User = require("./user.js");
  
-// имя текущего пользователя
-const userName = os.userInfo().username;
- 
-console.log(`Дата запроса: ${greeting.date}`);
-greeting.printMessage(userName);
+const eugene = new User("Rinat", 46);
+eugene.print();
+eugene.sayHi();
