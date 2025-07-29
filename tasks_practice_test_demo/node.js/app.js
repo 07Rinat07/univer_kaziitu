3c9bb1fa4f47dc9e4e0bb7e925ed2252988435c1
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-29 20:52:43 +0500

    Node js ---- Структура модулей

--- tasks_practice_test_demo/node.js/app.js
+++ tasks_practice_test_demo/node.js/app.js
@@ -1,9 +1,4 @@
-const greeting1 = require("./greeting.js");
-console.log(`Hello ${greeting1.name}`); //Hello Alice
- 
-const greeting2 = require("./greeting.js");
-greeting2.name= "Bob";
- 
-console.log(`Hello ${greeting2.name}`); //Hello Bob
-// greeting1.name тоже изменилось
-console.log(`Hello ${greeting1.name}`); //Hello Bob
+const messages = require("./messages");
+  
+messages.getMorningMessage();
+messages.getEveningMessage();
