82b0f2d2a93906be9a3692e41d177e92be640a40
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-29 20:37:20 +0500

    Основы работы с Node.js---Модули

--- tasks_practice_test_demo/node.js/app.js
+++ tasks_practice_test_demo/node.js/app.js
@@ -1,8 +1,4 @@
-const http = require("http");
-http.createServer(function(request,response){
-     
-    response.end("Hello NodeJS!");
-     
-}).listen(3000, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-});
+
+const greeting = require("./greeting")
+
+
