6f695dac9b6e85ace8cc92c8b047aa4567e28dcd
Rinat Sarmuldin <ura07srr@gmail.com>
2025-07-29 20:42:30 +0500

    Node js ---- Экспорт из модуля

--- tasks_practice_test_demo/node.js/greeting.js
+++ tasks_practice_test_demo/node.js/greeting.js
@@ -1,1 +1,13 @@
-console.log("Hello test node js.COM");
+const currentDate = new Date();
+exports.date = currentDate;  // экспортируем константу currentDate под именем date
+ 
+// экспортируем функцию
+exports.printMessage = function (name){
+    const hour = currentDate.getHours();
+    if(hour > 16)
+        console.log("Добрый вечер,", name);
+    else if(hour > 10)
+        console.log("Добрый день,", name);
+    else
+        console.log("Доброе утро,", name);
+}
